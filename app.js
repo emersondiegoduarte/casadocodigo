6235f7c4b9ebd682a2104c22849c7b9bab23ca2b
emerson.diego.duarte <emerson.diego.duarte@gmail.com>
2016-09-20 19:36:25 -0300

    Organizando código com módulos e movendo pastas referente a rotas e view para pasta app

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-
-var express = require('express');
-var porta = 3000
+var porta = 3000;
 var ip = 'localhost';
-var app = express();
-
-app.set("view engine", "ejs");
+var app = require("./config/express")();
+var rotasProduto = require("./app/routers/produtos")(app);
 
 app.listen(porta,function(){
   //response.send("<html><body>Listando Produtos express</body></html>")
   console.log("Servidor rodando em " + ip + " na porta " + porta);
 });
 
-app.get("/produtos",function(req,res){
-  res.render("produtos/lista");
-})
+
 
 
 
