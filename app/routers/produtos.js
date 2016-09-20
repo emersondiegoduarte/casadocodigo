526a2decd1d703e90a40477cc8881fde4e3e7c0c
emerson.diego.duarte <emerson.diego.duarte@gmail.com>
2016-09-20 20:32:09 -0300

    Configurando o Postgres

--- app/routers/produtos.js
+++ app/routers/produtos.js
@@ -1,6 +1,14 @@
 
 module.exports = function(app){
 	app.get("/produtos",function(req,res){
-	  res.render("produtos/lista");
+		  //res.render("produtos/lista");
+		  var postgresql  = require("pg");
+		  var conString = "postgres://postgres:root@localhost:5432/casadocodigo";
+		  var client = new postgresql.Client(conString);
+		  client.connect();
+		  client.query("SELECT * FROM livros", function(error,results){
+		   		res.send(results);
+		   });
+		  client.end();
 	});
 }; 
